b842088d603d8a5926b9406953a9af03c1e937d0
alejandrov114 <alejandro.villa42991@ucaldas.edu.co>
2025-06-01 15:14:47 -0500

    Se agrega el package formulariosSwing para las interfaces visuales

--- helpdesklite/src/main/java/ticketera/administrador.java
+++ helpdesklite/src/main/java/ticketera/administrador.java
@@ -1,4 +1,17 @@
 package ticketera;
 
 public class administrador {
+    private String nombre;
+
+    public administrador(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- helpdesklite/src/main/java/ticketera/main.java
+++ helpdesklite/src/main/java/ticketera/main.java
@@ -1,4 +1,8 @@
 package ticketera;
 
 public class main {
+    public static void main(String[] args) {
+        administrador admin1 = new administrador("Felipe");
+
+    }
 }
