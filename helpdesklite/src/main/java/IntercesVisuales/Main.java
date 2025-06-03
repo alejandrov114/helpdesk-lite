2657b0fa1f38fdd5537e3377d3bc54c632764ca4
Toro <juan.toro53367@ucaldas.edu.co>
2025-06-02 19:04:18 -0500

    Agrego interfaz Pantalla

--- helpdesklite/src/main/java/IntercesVisuales/Main.java
+++
@@ -1,4 +0,0 @@
-package IntercesVisuales;
-
-public class Main {
-}
--- helpdesklite/src/main/java/ticketera/main.java
+++ helpdesklite/src/main/java/ticketera/main.java
@@ -1,8 +1,16 @@
 package ticketera;
 
+import IntercesVisuales.Pantalla;
+
 public class main {
     public static void main(String[] args) {
         administrador admin1 = new administrador("Felipe");
-
+        
+        
+ // llamar login                                                                                                                   
+     Pantalla panta = new Pantalla ();
+     panta.setVisible(true); 
+     panta.setLocationRelativeTo(null); 
+    
     }
 }
