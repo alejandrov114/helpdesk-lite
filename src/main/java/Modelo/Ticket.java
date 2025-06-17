1f79c9d9d0570a32e5757e2c0343c0d67685d9c0
alejandrov114 <alejandro.villa42991@ucaldas.edu.co>
2025-06-17 14:29:21 -0500

    Se crea capa de encapsulamiento en la clase Ticket

--- src/main/java/Modelo/Ticket.java
+++ src/main/java/Modelo/Ticket.java
@@ -15,4 +15,82 @@ package Modelo;
  */
 public class Ticket {
     
+    private int id;
+    private String nombre;
+    private String correo;
+    private String descripcion;
+    private String prioridad;
+    private String estado;
+
+    //Constructor Vacio
+    public Ticket() {
+    }
+    
+    //Constructor con atributos
+
+    public Ticket(int id, String nombre, String correo, String descripcion, String prioridad, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    @Override
+    public String toString(){
+        return "Ticket # " + id + " [ " + prioridad + " ] - " + estado;
+    }
+    
+    
+    
+    
 }
